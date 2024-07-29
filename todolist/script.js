422e2b34742aedacd1b749dd30ac71a1fa819b4c
심현진 <hyunjin.dev143@gmail.com>
2024-07-29 22:04:15 +0900

    feat : 투두 입력 기능 추가

--- todolist/script.js
+++ todolist/script.js
@@ -2,10 +2,20 @@ const todoForm = document.getElementById('todoForm');
 const todoInput = document.querySelector('#todoForm input');
 const todoList = document.getElementById('todoList');
 
+function paintTodo(newTodo) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    li.appendChild(span);
+    span.innerText = newTodo;
+    todoList.appendChild(li);
+    console.log(li);
+}
+
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = '';
+    paintTodo(newTodo);
 }
 
 todoForm.addEventListener('submit', handleTodoSubmit);
