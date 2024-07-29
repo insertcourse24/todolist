ee30340fd65ea41f57648c83c4293e7bc71d979b
kwon5700 <kwon777071@gmail.com>
2024-07-29 14:46:44 +0900

    Feat : 1단계 계산기 만들기

--- practice/practice1.js
+++ practice/practice1.js
@@ -1,1 +1,11 @@
-// Mission 1
+let op = prompt()
+let n1 = parseInt(prompt())
+let n2 = parseInt(prompt())
+let result
+
+if(op == '+') result = n1+n2
+else if(op == '-') result = n1-n2
+else if(op == '*') result = n1*n2
+else if(op == '/') result = n1/n2
+
+alert(`${n1}${op}${n2}는 ${result}입니다.`)
