d539888540886f29a28000652ec9a7dcc3ae52d2
심현진 <hyunjin.dev143@gmail.com>
2024-07-29 22:27:23 +0900

    feat : 투두 저장 기능 추가

--- todolist/script.js
+++ todolist/script.js
@@ -10,35 +10,42 @@ function saveTodos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
 
-function deleteTodo(event) {
+function deleteTodo() {
     const li = event.target.parentElement;
     li.remove();
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+    saveTodos();
 }
 
 function paintTodo(newTodo) {
     const li = document.createElement('li');
+    li.id = newTodo.id;
     const span = document.createElement('span');
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const btn = document.createElement('button');
     btn.innerText = 'X';
     btn.addEventListener('click', deleteTodo);
     li.appendChild(span);
     li.appendChild(btn);
+    span.innerText = newTodo.text;
     todoList.appendChild(li);
-    console.log(li);
 }
 
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = '';
-    todos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    todos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveTodos();
 }
 
 todoForm.addEventListener('submit', handleTodoSubmit);
-
+// 2단계 - localStorage에서 Todos 불러오기
 const savedTodos = localStorage.getItem(TODOS_KEY);
 
 if (savedTodos) {
