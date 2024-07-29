a0a4dc41375d05553c4042fc96240612fd2ad4ef
심현진 <hyunjin.dev143@gmail.com>
2024-07-29 15:48:52 +0900

    feat : 미션1 1단계 계산기 기능 구현

--- practice/practice1.js
+++ practice/practice1.js
@@ -1,1 +1,22 @@
 // Mission 1
+// 1단계 - 계산기 만들기
+let num1 = prompt('첫번째 수를 입력하세요.');
+const yeonsan = prompt('연산자 (+, -, *, /)를 입력하세요.');
+let num2 = prompt('두번째 수를 입력하세요.');
+
+const intNum1 = parseInt(num1);
+const intNum2 = parseInt(num2);
+
+if(yeonsan === '+'){
+    alert(`${num1} + ${num2}는 ${intNum1 + intNum2} 입니다.`);
+} else if(yeonsan === '-'){
+    alert(`${num1} - ${num2}는 ${intNum1 - intNum2} 입니다.`);
+} else if(yeonsan === '*'){
+    alert(`${num1} * ${num2}는 ${intNum1 * intNum2} 입니다.`);
+} else if(yeonsan === '/'){
+    if(num2 === 0){
+        alert("0으로는 나눌 수 없습니다.");
+    } else {
+        alert(`${num1} / ${num2}는 ${intNum1 / intNum2} 입니다.`);
+    }
+}
