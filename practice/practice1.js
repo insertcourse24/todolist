643c4d0cb2cdcac2e20a51a3158433969733420c
kwon5700 <kwon777071@gmail.com>
2024-07-29 14:52:49 +0900

    Feat : 2단계 함수로 분리하기

--- practice/practice1.js
+++ practice/practice1.js
@@ -1,11 +1,23 @@
-let op = prompt()
-let n1 = parseInt(prompt())
-let n2 = parseInt(prompt())
+let op
+let n1
+let n2
 let result
 
-if(op == '+') result = n1+n2
-else if(op == '-') result = n1-n2
-else if(op == '*') result = n1*n2
-else if(op == '/') result = n1/n2
+input()
+calculate()
 
-alert(`${n1}${op}${n2}는 ${result}입니다.`)
+function input() {
+    op = prompt()
+    n1 = parseInt(prompt())
+    n2 = parseInt(prompt())
+    result
+}
+
+function calculate() {
+    if(op == '+') result = n1+n2
+    else if(op == '-') result = n1-n2
+    else if(op == '*') result = n1*n2
+    else if(op == '/') result = n1/n2
+    
+    alert(`${n1}${op}${n2}는 ${result}입니다.`)
+}
