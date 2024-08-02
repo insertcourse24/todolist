fb1d54cbb334cb950f00c0606d738fcca31e754b
kwon5700 <kwon777071@gmail.com>
2024-08-02 23:57:07 +0900

    Feat : 일정 개수 변경 tasks 개발

--- todolist/script.js
+++ todolist/script.js
@@ -12,19 +12,34 @@ today.innerText = `${week[week_index-1]}, ${day} ${month[month_index]}`
 
 const insert = document.getElementById('insert_button')
 
+let count = 0
+
  insert.addEventListener('click', () => {
+    // 일정 추가 코드
     let todo = prompt("오늘 할 일을 입력해주세요!")
 
     const text = document.createElement('div')
     const div = document.querySelector('#text_box')
 
     text.innerHTML = 
-    `<div class="todo_list">
-        <div class="finish_checkBox"></div>
-        <h1>${todo}</h1>
-    </div>`
+        `
+            <div class="todo_list">
+                <div class="finish_checkBox"></div>
+                <h1>${todo}</h1>
+            </div>
+            <div class="underbar">
+        `
 
     div.appendChild(text)
+
+    // 일정 개수 변경 코드
+    count+=1
+    const task = document.getElementById('tasks_h2')
+
+    task.innerText = `${count} tasks`
+
+
+    
 
  })
 
