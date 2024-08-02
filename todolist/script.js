402b0c5c036ed700788da4cd4610230629032854
kwon5700 <kwon777071@gmail.com>
2024-08-02 23:27:43 +0900

    Feat : 일정 추가 기능 개발

--- todolist/script.js
+++ todolist/script.js
@@ -10,11 +10,25 @@ const month_index = date.getMonth()
 
 today.innerText = `${week[week_index-1]}, ${day} ${month[month_index]}`
 
-function insert() {
+const insert = document.getElementById('insert_button')
+
+ insert.addEventListener('click', () => {
     let todo = prompt("오늘 할 일을 입력해주세요!")
 
-    
-}
+    const text = document.createElement('div')
+    const div = document.querySelector('#text_box')
+
+    text.innerHTML = 
+    `<div class="todo_list">
+        <div class="finish_checkBox"></div>
+        <h1>${todo}</h1>
+    </div>`
+
+    div.appendChild(text)
+
+ })
+
+
 
 
 
