e5263ae26f5e93ae147788c6309fbc30c4d31587
kwon5700 <kwon777071@gmail.com>
2024-08-03 13:07:55 +0900

    Fix : 성공 삭제 버튼 위치 오류 수정

--- todolist/script.js
+++ todolist/script.js
@@ -1,3 +1,4 @@
+// 오늘의 날짜를 출력하기 위한 코드
 const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
@@ -18,31 +19,33 @@ let count = 0
     // 일정 추가 코드
     let todo = prompt("오늘 할 일을 입력해주세요!")
 
-    const text = document.createElement('div')
-    const div = document.querySelector('#text_box')
-
-    text.innerHTML = 
-        `
-            <div class="todo_list">
-                <div class="finish_checkBox"></div>
-                <h1>${todo}</h1>
-            </div>
-            <div class="underbar">
-        `
-
-    div.appendChild(text)
-
-    // 일정 개수 변경 코드
-    count+=1
-    const task = document.getElementById('tasks_h2')
-
-    task.innerText = `${count} tasks`
-
-
+    if(!todo.length) alert('공백은 입력이 불가합니다!')
+    else {
+        const text = document.createElement('div')
+        const div = document.querySelector('#text_box')
     
+        text.innerHTML = 
+            `
+                <div class="todo_list">
+                    <div class="finish_checkBox"></div>
+                    <h1>${todo}</h1>
+                    <div class="success">Success</div>
+                    <div class="delete">Delete</div>
+                </div>
+                <div class="underbar">
+            `
+    
+        div.appendChild(text)
 
+        // 일정 개수 변경 코드
+        count+=1
+        const task = document.getElementById('tasks_h2')
+    
+        task.innerText = `${count} tasks`
+    }
  })
 
+ 
 
 
 
